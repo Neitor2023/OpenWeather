7d554eadf509f375972379c421d051c03df9ba0f
Romy Garcia <neitorme@gmail.com>
2023-03-04 14:53:20 -0500

    LoaderMain

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 
+import { load } from './components/load.js'
+
+window.addEventListener('load', function () {
+    load()
+})
+
 ReactDOM.createRoot(document.getElementById('root')).render(
     <App />,
 );
