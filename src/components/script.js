4f8899fd2afc3b4fe7481dfe7cfe003f9721891f
Romy Garcia <neitorme@gmail.com>
2023-03-04 20:58:45 -0500

    IndicadorDeCarga

--- src/components/script.js
+++ src/components/script.js
@@ -1,8 +1,6 @@
-export function loader () {
-  window.addEventListener('load', () => {
-    console.log('llegue')
+console.log('lleguee')
+window.addEventListener('load'), () => {
     const container_loader = document.querySelector('.container_loader')
     container_loader.style.opacity = 0
     container_loader.style.visibility = 'hidden'
-})
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+// import Loader from './components/loader'
 
-import { load } from './components/load.js'
-
-window.addEventListener('load', function () {
-    load()
+window.addEventListener('load', () => {
+    const container_loader = document.querySelector('.container_loader')
+    container_loader.style.opacity = 0
+    container_loader.style.visibility = 'hidden'
 })
 
 ReactDOM.createRoot(document.getElementById('root')).render(
