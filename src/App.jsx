566886ddf04233e4de4b06478d5b187c25e10e42
Romy Garcia <neitorme@gmail.com>
2023-03-05 23:50:09 -0500

    ListoFormulario

--- src/App.jsx
+++ src/App.jsx
@@ -1,53 +1,34 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import WeatherInfo from './components/WeatherInfo';
 import WeatherForm from './components/WeatherForm';
 import { WEATHER_KEY } from './keys'
 import axios from 'axios'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [weatherData, setweatherData] = useState({})
 
-  const getWeather = async e => {
+  const getWeather = e => {
     e.preventDefault();
     const { city, country } = e.target.elements;
     const cityValue = city.value
     const countryValue = country.value
-    
-    // const API_URL = `http://api.openweathermap.org/data/2.5/weather?q=${cityValue},${countryValue}&appid=${WEATHER_KEY}&units=metric`;
-    
-    // const response = await fetch(API_URL);
-    // const data = await response.json();
-
-    // console.log(data)
-    // console.log(API_URL)
-    // console.log(state)
-
-    // useState({
-        // country: data.sys.country,
-        // city: data.main.name
-        // temperature: data.main.temp,
-        // description: data.weather[0].description,
-        // humidity: data.main.humidity,
-        // wind_speed: data.wind.speed,
-        // error: null
-      // });
-    useEffect(() => {
+    if (cityValue && countryValue) {
       axios
-      .get(`http://api.openweathermap.org/data/2.5/weather?q=${cityValue},${countryValue}&appid=${WEATHER_KEY}`)
-      .then((resp) => resp.data)
-      console.log(resp.data)
-      .catch((error) => console.error(error));
-    }, []);
-}
+        .get(`http://api.openweathermap.org/data/2.5/weather?q=${cityValue},${countryValue}&appid=${WEATHER_KEY}`)
+        .then((resp) => setweatherData(resp.data))
+        .catch((error) => setweatherData());
+    } else {
+      setweatherData()
+      // setData2({error:"Please Enter a City and a Country"});
+    }
+  };
 
-    console.log(data)
-  
   return (
     <div className='container p-4'>
       <div className='row'>
         <div className="col-md-4 mx-auto">
-          <WeatherForm  getWeather={getWeather}/>
-          <WeatherInfo/>
+          <WeatherForm getWeather={getWeather} />
+          <WeatherInfo data={weatherData} />
         </div>
       </div>
     </div>
--- src/components/WeatherInfo.jsx
+++ src/components/WeatherInfo.jsx
@@ -1,9 +1,52 @@
-const WeatherInfo = () => {
+import { setState } from 'react';
+const WeatherInfo = ({ data }) => {
     return (
-        <div>
-            WeatherInfo
+        <div className="card card-body">
+            {data && <div>
+                <p><span>Coutry: </span>{data.name}</p><br />
+                <p><span>Temperature: </span> {data.weather?.[0].description} C, {data.main?.temp}</p><br />
+                <p><span>Humidity: </span>{data.main?.humidity}</p><br />
+                <p><span>Wind Speed: </span>{data.wind?.speed}</p><br />
+            </div>}
+            {!data && <div>
+                <p><span> </span></p><br />
+                <p><span> </span></p><br />
+                <p><span> </span></p><br />
+                <p><span> </span></p><br />
+            </div>}
         </div>
     )
 }
 
 export default WeatherInfo
+
+{/* <div className="alert alert-danger">{!data && <div><p>Please Enter a City and a Country</p></div>}</div>, */}
+
+{/* <div className="alert alert-danger">{!data && <div><p>Please Enter a City and a Country</p></div>}</div>,
+<div className="card card-body">
+    {data && <div>
+        <p><span>Coutry: </span>{data.name}</p><br/>
+        <p><span>Temperature: </span> {data.weather?.[0].description} C, {data.main?.temp}</p><br />
+        <p><span>Humidity: </span>{data.main?.humidity}</p><br />
+        <p><span>Wind Speed: </span>{data.wind?.speed}</p><br />
+    </div>}
+    {!data && <div>
+        <p><span> </span></p><br/>
+        <p><span> </span></p><br />
+        <p><span> </span></p><br />
+        <p><span> </span></p><br />
+    </div>}
+</div> */}
+
+{/* <div className="card card-body">            
+{data.name && <div><p><span>Coutry: </span>{data.name}</p><br/></div>}
+{data.weather?.[0].description && <div><p><span>Temperature: </span>{data.weather?.[0].description} C, {data.main?.temp}</p><br/></div>}
+{data.main?.humidity && <div><p><span>Humidity: </span>{data.main?.humidity}</p><br/></div>}
+{data.wind?.speed && <div><p><span>Wind Speed: </span>{data.wind?.speed}</p><br/></div>}
+
+{!data.name && <div><p><span> </span></p><br/></div>}
+{!data.weather?.[0].description && <div><p><span> </span></p><br/></div>}
+{!data.main?.humidity && <div><p><span> </span></p><br/></div>}
+{!data.wind?.speed && <div><p><span> </span></p><br/></div>}
+
+</div> */}
